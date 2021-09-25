09b851bb95ba92088181fb982e095fed7b95f2c2
johnoh <johnoh@usc.edu>
2021-09-24 23:23:53 -0400

    Pushing basic skeleton of html, java, and css

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+$("#currentDay").text(moment().format("MMM DD, YYYY"));
+var currentTime = moment()
+
+
+// function storage () {
+//     for (var i = 0; i <= hours.length; i++) {
+    
+// }
+
+
+
+//identify the id and whatever moment has an id 
